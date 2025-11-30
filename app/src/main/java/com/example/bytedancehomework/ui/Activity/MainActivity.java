61eb2f15de252de3f6ec3f66bf5e524927937d39
SiYouDi <1224654165@qq.com>
2025-11-30 16:14:28 +0800

    refactor:使用本地视频进行测试

--- app/src/main/java/com/example/bytedancehomework/ui/Activity/MainActivity.java
+++ app/src/main/java/com/example/bytedancehomework/ui/Activity/MainActivity.java
@@ -192,7 +192,7 @@ public class MainActivity extends AppCompatActivity
         // 单列视频
         sampleItems.add(new FeedItem("单列视频标题",
                 "这是单列布局的视频内容描述",
-                "http://117.72.208.2/videos/test.mp4",
+                "android.resource://" + getPackageName() + "/" + R.raw.test_video,
                 "https://example.com/video_cover1.jpg",
                 1920, 1080, 120000, LayoutMode.single));
 
@@ -205,7 +205,7 @@ public class MainActivity extends AppCompatActivity
         // 网格视频
         sampleItems.add(new FeedItem("网格视频标题",
                 "这是网格布局的视频内容",
-                "http://117.72.208.2/videos/test.mp4",
+                "android.resource://" + getPackageName() + "/" + R.raw.test_video,
                 "https://example.com/video_cover2.jpg",
                 1280, 720, 90000, LayoutMode.grid));
 
